e8d1e5128d21a8cf88fbb149cb974c013bd35496
denis <denis@DENISTUF>
2023-10-19 12:02:42 +0300

    check

---
+++ LinqQuerySyntax/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        // string collection
+        IList<string> stringList = new List<string>() {
+            "C# Tutorials",
+            "VB.NET Tutorials",
+            "Learn C++",
+            "MVC Tutorials" ,
+            "Java"
+        };
+
+        // LINQ Query Syntax
+        var result = from s in stringList
+                     where s.Contains("Tutorials")
+                     select s;
+
+        foreach (var str in result)
+        {
+            Console.WriteLine(str);
+        }
+        Console.ReadKey();
+    }
+}
