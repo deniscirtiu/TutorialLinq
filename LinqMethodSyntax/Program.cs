7557584992f6d9482f18218bd4b0c6bb7876bf66
denis.cirtiu <denis.cirtiu@programming-pool.com>
2023-10-19 14:50:32 +0300

    Linq Method Syntax

---
+++ LinqMethodSyntax/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Program
+{
+	public static void Main()
+	{
+		// string collection
+		IList<string> stringList = new List<string>() {
+			"C# Tutorials",
+			"VB.NET Tutorials",
+			"Learn C++",
+			"MVC Tutorials" ,
+			"Java"
+		};
+
+		// LINQ Method Syntax
+		var result = stringList.Where(s => s.Contains("Tutorials"));
+
+
+		foreach (var str in result)
+		{
+			Console.WriteLine(str);
+		}
+
+		Console.ReadKey();
+	}
+}
