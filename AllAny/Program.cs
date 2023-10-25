62202d1e688d66e08945fba703f2a603c3167ba0
denis <denis@DENISTUF>
2023-10-25 16:36:48 +0300

    all any

---
+++ AllAny/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        IList<Student> studentList = new List<Student>() {
+            new Student() { StudentID = 1, StudentName = "John", Age = 13 } ,
+            new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 } ,
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+        };
+
+        // checks whether all the students are teenagers    
+        bool areAllStudentsTeenAger = studentList.All(s => s.Age > 12 && s.Age < 20);
+
+
+        Console.WriteLine(areAllStudentsTeenAger);
+        Console.ReadKey();
+    }
+}
+
+public class Student
+{
+
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public int Age { get; set; }
+}
