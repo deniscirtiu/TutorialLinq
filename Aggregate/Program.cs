faa97123a03877257826c803641e41ba08f5d527
denis <denis@DENISTUF>
2023-10-25 17:44:28 +0300

    initial aggregate project

---
+++ Aggregate/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        IList<String> strList = new List<String>() { "One", "Two", "Three", "Four", "Five" };
+
+        var commaSeperatedString = strList.Aggregate((s1, s2) => s1 + ", " + s2);
+
+        Console.WriteLine(commaSeperatedString);
+        Console.ReadKey();
+    }
+}
