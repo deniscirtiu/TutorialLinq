66b5e1ae02ce8591f3dec73019ce156721fcc589
denis <denis@DENISTUF>
2023-10-26 17:06:16 +0300

    count

---
+++ Count/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        IList<Student> studentList = new List<Student>() {
+            new Student() { StudentID = 1, StudentName = "John", Age = 13 } ,
+            new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 },
+            new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 },
+            new Student() { StudentID = 4, StudentName = "Ram" , Age = 20 },
+            new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+        };
+
+        var totalStudents = studentList.Count();
+
+        Console.WriteLine("Total Students: {0}", totalStudents);
+
+        var adultStudents = studentList.Count(s => s.Age >= 18);
+
+        Console.WriteLine("Number of Adult Students: {0}", adultStudents);
+        Console.ReadKey();
+    }
+}
+
+public class Student
+{
+
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+    public int Age { get; set; }
+}
