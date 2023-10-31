022014465ff1918612b3922b7b83dcd164eb5364
denis <denis@DENISTUF>
2023-10-31 12:56:26 +0200

    DefaultIfEmpty

---
+++ DefaultIfEmpty/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultIfEmpty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> emptyList = new List<string>();
+
+            var newList1 = emptyList.DefaultIfEmpty();
+            var newList2 = emptyList.DefaultIfEmpty("None");
+
+            Console.WriteLine("Count: {0}", newList1.Count());
+            Console.WriteLine("Value: {0}", newList1.ElementAt(0));
+
+            Console.WriteLine("Count: {0}", newList2.Count());
+            Console.WriteLine("Value: {0}", newList2.ElementAt(0));
+
+            IList<int> emptyList1 = new List<int>();
+
+            var emptyList2 = emptyList1.DefaultIfEmpty();
+            var emptyList3 = emptyList1.DefaultIfEmpty(100);
+
+            Console.WriteLine("Count: {0}", newList1.Count());
+            Console.WriteLine("Value: {0}", newList1.ElementAt(0));
+
+            Console.WriteLine("Count: {0}", newList2.Count());
+            Console.WriteLine("Value: {0}", newList2.ElementAt(0));
+
+
+            Console.ReadLine();
+        }
+    }
+}
