54269a38471ed47986d6499c4d878316aed249c8
denis <denis@DENISTUF>
2023-10-31 12:50:05 +0200

    concat

---
+++ Concat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> collection1 = new List<string>() { "One", "Two", "Three" };
+            IList<string> collection2 = new List<string>() { "Five", "Six" };
+
+            var collection3 = collection1.Concat(collection2);
+
+            foreach (var item in collection3)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
