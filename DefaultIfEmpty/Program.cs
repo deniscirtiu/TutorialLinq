8ace18419f9ecc1678e473c9f582307be875dcdb
denis <denis@DENISTUF>
2023-10-31 13:01:22 +0200

    Complex DefaultIfEmpty

--- DefaultIfEmpty/Program.cs
+++ DefaultIfEmpty/Program.cs
@@ -32,6 +32,23 @@ namespace DefaultIfEmpty
             Console.WriteLine("Count: {0}", newList2.Count());
             Console.WriteLine("Value: {0}", newList2.ElementAt(0));
 
+            Console.WriteLine("-----------------------------------Complex DefaultIfEmpty-------------------------------");
+
+            IList<Student> emptyStudentList = new List<Student>();
+
+            var newStudentList1 = emptyStudentList.DefaultIfEmpty(new Student());
+
+            var newStudentList2 = emptyStudentList.DefaultIfEmpty(new Student()
+            {
+                StudentID = 0,
+                StudentName = ""
+            });
+
+            Console.WriteLine("Count: {0} ", newStudentList1.Count());
+            Console.WriteLine("Student ID: {0} ", newStudentList1.ElementAt(0));
+
+            Console.WriteLine("Count: {0} ", newStudentList2.Count());
+            Console.WriteLine("Student ID: {0} ", newStudentList2.ElementAt(0).StudentID);
 
             Console.ReadLine();
         }
---
+++ DefaultIfEmpty/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace DefaultIfEmpty
+{
+    internal class Student
+    {
+        public int StudentID { get; internal set; }
+        public string StudentName { get; internal set; }
+    }
+}
