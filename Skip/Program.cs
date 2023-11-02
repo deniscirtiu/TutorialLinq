2492c6d0fdeec98c00ef2daf4603a07b6ebcc7fe
denis.cirtiu <denis.cirtiu@programming-pool.com>
2023-11-02 16:40:12 +0200

    skip skipwhile take takewhile

---
+++ Skip/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		IList<string> strList = new List<string>() { "One", "Two", "Three", "Four", "Five" };
+
+		var result = strList.Skip(2);
+
+		foreach (var str in result)
+			Console.WriteLine(str);
+        Console.WriteLine("============Skip While=============");
+		IList<string> strList2 = new List<string>() {
+											"One",
+											"Two",
+											"Three",
+											"Four",
+											"Five",
+											"Six"  };
+
+		var resultList = strList2.SkipWhile(s => s.Length < 4);
+
+		foreach (string str in resultList)
+			Console.WriteLine(str);
+
+		Console.ReadLine();
+
+	}
+}
---
+++ TakeTakeWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+	public static void Main()
+	{
+		IList<string> strList = new List<string>() { "One", "Two", "Three", "Four", "Five" };
+
+		var newList = strList.Take(2);
+
+		foreach (var str in newList)
+			Console.WriteLine(str);
+
+        Console.WriteLine("=========TakeWhile=========");
+
+		IList<string> strList2 = new List<string>() { "Three", "Four", "Five", "Hundred" };
+
+		var newList2 = strList2.TakeWhile(s => s.Length > 4);
+
+		foreach (var str in newList)
+			Console.WriteLine(str);
+
+		Console.ReadLine();	
+
+	}
+}
