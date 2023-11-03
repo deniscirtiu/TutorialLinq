60132e2e6756272b0291df87d5e426b9e8da25ad
denis <denis@DENISTUF>
2023-11-03 16:18:24 +0200

    ToOperators

---
+++ ToOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        IList<string> strList = new List<string>() {
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Three"
+        };
+
+        Console.WriteLine("strList type: {0}", strList.GetType().Name);
+
+        string[] strArray = strList.ToArray<string>();// converts List to Array
+
+        Console.WriteLine("strArray type: {0}", strArray.GetType().Name);
+
+        IList<string> newList = strArray.ToList<string>(); // converts array into list
+
+        Console.WriteLine("newList type: {0}", newList.GetType().Name);
+
+        Console.ReadKey();
+    }
+}
